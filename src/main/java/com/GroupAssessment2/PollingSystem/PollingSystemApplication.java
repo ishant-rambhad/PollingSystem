191501cfa1cfded27bbd4057f560b364410508f8
ISHANT HEMRAJ RAMBHAD <118370814+ishant-rambhad@users.noreply.github.com>
2024-09-19 15:29:16 +0530

    Initial commit

---
+++ src/main/java/com/GroupAssessment2/PollingSystem/PollingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.GroupAssessment2.PollingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GroupAssessment2/PollingSystem/PollingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GroupAssessment2.PollingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
