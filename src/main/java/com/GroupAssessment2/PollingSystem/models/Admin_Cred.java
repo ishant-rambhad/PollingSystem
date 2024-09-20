8473be068f6bb9bcd19223ad76c662c7184c71a8
ISHANT HEMRAJ RAMBHAD <118370814+ishant-rambhad@users.noreply.github.com>
2024-09-20 17:59:55 +0530

    background update in user and admin login page

---
+++ src/main/java/com/GroupAssessment2/PollingSystem/models/Admin_Cred.java
@@ -0,0 +1,52 @@
+package com.GroupAssessment2.PollingSystem.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Admin_Cred")
+public class Admin_Cred {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    // Constructors
+    public Admin_Cred() {}
+
+    public Admin_Cred(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
